4b75768b13bf92754946e145f7e91efc006c2dc5
Gabriel Garcia <g4brielgarci4@gmail.com>
2023-11-22 21:39:39 -0300

    Improve code readability and depuration

---
+++ Expansão do Hakari/gui_da_expansão.py
@@ -0,0 +1,202 @@
+import tkinter as tk
+import random
+from PIL import Image, ImageTk
+from lógica_da_expansão import SimulateExpansion
+
+EXPANSION = SimulateExpansion()
+
+class GameGui:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Hakari's Domain Expansion")
+        self.current_dance_image = None
+        self.current_scenario = 0
+
+        self.setup_gui()
+
+    #Setting elements
+    def setup_gui(self): 
+        self.background = tk.Label(root, width=500, height=500) 
+        self.background.place(x = 0, y = 0)
+
+        self.canvas = tk.Canvas(root, width=80, height=80)
+        self.canvas.pack()
+
+        self.message = tk.Label(root, text=EXPANSION.message)
+        self.tries_message = tk.Label(root, text=f"Tentativas desde o último Jackpot: {EXPANSION.tries}")
+        self.message.pack()
+        self.tries_message.pack()
+
+        self.slots = tk.Label(root, text=f"| {EXPANSION.slots[0]} | | {EXPANSION.slots[1]} | | {EXPANSION.slots[2]} |")
+        self.slots.pack()
+
+        self.spin_button = tk.Button(root, text="Spin", command=lambda: self.spin(None), width=5, height=1)
+        self.spin_button.place(x=205, y=400)
+        self.root.bind("<KeyPress-s>", self.spin)
+
+        self.riichi_button = tk.Button(root, text="Riichi", command=lambda: self.riichi(None), width=5, height=1)
+        self.riichi_button.place(x=255, y=400)
+        self.root.bind("<KeyPress-r>", self.riichi)
+
+        self.load_scenarios()
+        self.load_dance_images()
+        self.create_dance_label()
+        self.update_scenario()
+
+    #Setting images
+    def load_dance_images(self):
+        #Loading Dance Images
+        images = [Image.open(f"Expansão do Hakari\dance images\dance_{i}.png").resize((150, 150)) for i in range(1, 4)]
+        self.dance_images = [ImageTk.PhotoImage(img) for img in images]
+
+    def create_dance_label(self):
+        #Creating Dance Label
+        self.dance = tk.Label(self.root, image=self.dance_images[0], width=150, height=150)
+        self.dance.image = self.dance_images[0]
+        self.image_index = 0
+        self.jackpot_animating = False
+
+    def load_scenarios(self):
+        #Loading Scenario Images
+        images = [Image.open(f"Expansão do Hakari/scenarios/scenario({i}).png").resize((500, 500)) for i in range(0, 9)]
+        self.scenarios = [ImageTk.PhotoImage(img) for img in images]
+    
+    def update_scenario(self):
+        #Updating Background Image
+        self.current_scenario = EXPANSION.scenario_id
+        self.background.config(image = self.scenarios[self.current_scenario])
+    
+    #Setting messsages
+    def message_update(self):
+        #Updating Main Message Label
+        self.message.config(text=EXPANSION.message)
+
+    def tries_message_update(self):
+        #Updating Tries Message Label
+        self.tries_message.config(text=f"Tentativas desde o último Jackpot: {EXPANSION.tries}")
+
+    #Game's main elements
+    def reset_game_elements(self):
+        #Reseting Main Game Elements
+        self.dance.place_forget()
+        self.jackpot_animating = False
+        self.canvas.delete("all")  
+
+    def spin(self, event=None):
+        #Spinning Logic
+        self.reset_game_elements()
+        EXPANSION.play()
+        self.message_update()
+        self.tries_message_update()
+        self.update_scenario()
+
+        final_numbers = [EXPANSION.slots[0], EXPANSION.slots[1], EXPANSION.slots[2]]
+
+        self.animate_slots(final_numbers)
+        self.animate_balls()
+
+    def riichi(self, event=None):
+        #Riichi Stage Logic
+        self.reroll_unset()
+        EXPANSION.set_scenario()
+        self.update_scenario()
+
+        if EXPANSION.slots[0] != 0:
+            self.update_last_number(0)
+        else:
+            self.message.config(text="Você precisa jogar se quiser ganhar!")
+
+    def reroll_set(self):
+        #Setting Reroll Elements
+        self.message_update()
+        self.spin_button.config(text="Sim!", command=lambda: self.reroll(True))
+        self.riichi_button.config(text="Não", command=lambda: self.reroll(False))
+        self.root.bind("<KeyPress-s>", lambda event: self.reroll(True))
+        self.root.bind("<KeyPress-r>", lambda event: self.reroll(False))
+
+    def reroll_unset(self):
+        #Unsetting Reroll Elements
+        self.spin_button.config(text="Spin", command=lambda: self.spin(None))
+        self.riichi_button.config(text="Riichi", command=lambda: self.riichi(None))
+        self.root.bind("<KeyPress-s>", self.spin)
+        self.root.bind("<KeyPress-r>", self.riichi)
+
+    def reroll(self, lucky):
+        #Reroll Logic
+        EXPANSION.result_change(lucky)
+        if lucky == True and EXPANSION.result_change_chance > 50:
+            self.riichi()
+        else:
+            self.message_update()
+            self.reroll_unset()
+                
+    #Setting animations    
+    def update_last_number(self, iteration):
+        self.message_update()
+        final_number = EXPANSION.slots[2]
+        animation_speed = 100
+        total_iterations = 20
+
+        if iteration < total_iterations:
+            random_number = random.choice(range(1, 8))
+            self.slots.config(text=f"| {EXPANSION.slots[0]} | | {EXPANSION.slots[1]} | | {random_number} |")
+            self.root.after(animation_speed, lambda: self.update_last_number(iteration + 1))
+        else:
+            self.slots.config(text=f"| {EXPANSION.slots[0]} | | {EXPANSION.slots[1]} | | {final_number} |")
+            EXPANSION.riichi()
+            self.message_update()
+            if EXPANSION.jackpot_reached == True:
+                self.animate_dance_images()
+                self.update_scenario()
+                self.canvas.delete("all")
+            elif EXPANSION.message == "Acha que pode mudar o jogo???":
+                self.reroll_set()
+
+    def animate_slots(self, final_numbers):
+        #Animating Spinning Slots
+        animation_speed = 50
+        total_iterations = 20
+
+        def update_numbers(iteration):
+            if iteration < total_iterations:
+                random_numbers = [random.choice(range(1, 8)) for _ in range(3)]
+                self.slots.config(text=f"| {random_numbers[0]} | | {random_numbers[1]} | | {random_numbers[2]} |")
+                self.root.after(animation_speed, lambda: update_numbers(iteration + 1))
+            else:
+                self.slots.config(text=f"| {final_numbers[0]} | | {final_numbers[1]} | | ? |")
+                self.message_update()
+
+        update_numbers(0)
+
+    def animate_balls(self):
+        #Animating Ball's Shoots
+        x1, y1 = 25, 25
+        animation_speed = 50
+
+        def move_ball(x, y):
+            self.canvas.delete("ball")
+            self.canvas.create_oval(x, y, x - 50, y + 50, fill=EXPANSION.ball_color, tags="ball")
+            x += 5
+            if x < 70:
+                self.root.after(animation_speed, lambda: move_ball(x, y))
+
+        move_ball(x1, y1)
+
+    def animate_dance_images(self):
+        #Winning Animation 
+        self.dance.place(x=175, y=240)
+        def toggle_image():
+            if self.jackpot_animating:
+                self.image_index = (self.image_index + 1) % len(self.dance_images)
+                self.current_dance_image = self.dance_images[self.image_index]
+                self.dance.config(image=self.current_dance_image)
+                self.root.after(120, toggle_image)
+
+        self.jackpot_animating = True
+        toggle_image()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = GameGui(root)
+    root.geometry("500x500")
+    root.mainloop()
--- Expansão do Hakari/lógica_da_expansão.py
+++ Expansão do Hakari/lógica_da_expansão.py
@@ -1,20 +1,28 @@
 import random
 
-class Simular_expansao:
+class SimulateExpansion:
     def __init__(self):
+        #Visual Elements
         self.cards = list(range(1, 8))
-        self.chance = list(range(1,101))
-        self.ball = None
-        self.shoot = 0
         self.slots = [0, 0, 0]
+        self.ball_color = None
+
+        #Chance Related Elements
+        self.jackpot_reached = False
+        self.current_jackpot_chance = 0
+        self.increasing_jackpot_chance = list(range(1,101))
+        self.result_change_chance = 0
+        
+        #Message Elements
         self.tries = 0
         self.message = "Se sente com sorte?"
-        self.jackpot = False
-        self.scenario = 0
-        self.result_change_odd = 0
 
-    def draw(self):
-        if self.ball == "purple":
+        #Background Image ID
+        self.scenario_id = 0
+
+    def draw_cards(self):
+        if self.ball_color == "purple":
+            #Guaranted Jackpot if ball color equals purple
             slot = random.choice(self.cards)
             for i in range(0, len(self.slots)):
                 self.slots[i] = slot
@@ -22,82 +30,110 @@ class Simular_expansao:
             for slot in range(0, len(self.slots)):
                 self.slots[slot] = random.choice(self.cards)
 
-    def chance_boost(self, felling_lucky):
+    def result_change(self, felling_lucky):
         if felling_lucky == True:
-            self.result_change_odd = random.choice(range(1, 101))
-            if self.result_change_odd <= 50:
+            self.result_change_chance = random.choice(range(0, 101))
+            if self.result_change_chance <= 30:
                 self.message = "Tá se achando demais kkkk"
-                self.chance = list(range(1,101))
+                self.result_change_chance = 0
                 self.slots = [0, 0, 0]
-            elif self.result_change_odd <= 80:
-                self.scenario = 5 #Yume Background
+            elif self.result_change_chance <= 50:
+                self.set_scenario()
                 self.slots[2] = random.choice(range(1, 8))
-                self.chance = list(range(1,101))
-                self.riichi()
-            elif self.result_change_odd <= 95:
-                self.scenario = 6 #Amanogawa Cut Scene
+                self.debug("result_change")
+                self.result_change_chance = 0
+            elif self.result_change_chance <= 75:
+                self.set_scenario("result_change")
                 self.slots[2] = random.choice(self.cards)
-                self.chance = list(range(1, 101))
-                self.riichi()
-            elif self.result_change_odd > 95:
-                self.scenario = 7 #Group Indicators
+                self.debug("result_change")
+                self.result_change_chance = 0
+            elif self.result_change_chance > 75:
+                self.set_scenario()
+                self.slots[1] = self.slots[0]
                 self.slots[2] = self.slots[0]
-                self.chance = list(range(1,101))
-                self.riichi()
+                self.debug("result_change")
+                self.increasing_jackpot_chance = list(range(1,101))
+                self.result_change_chance = 0
         else:
             self.message = "Resolveu Correr?"
 
     def play(self):
-        self.jackpot = False
-        self.scenario = 0
-        self.shoot = random.choice(self.chance)
+        self.jackpot_reached = False
+        self.scenario_id = 0
+        self.current_jackpot_chance = random.choice(self.increasing_jackpot_chance)
         if self.tries != 0:
-            self.chance = list(range(self.shoot, 101))
+            self.increasing_jackpot_chance = list(range(self.current_jackpot_chance, 101))
 
         self.tries += 1
 
-        if self.shoot <= 50:
-            self.ball = "green"
+        if self.current_jackpot_chance <= 50:
+            self.ball_color = "green"
             self.cards = list(range(1, 8))
-        elif self.shoot > 50 and self.shoot <= 75:
-            self.ball = "red"
+        elif self.current_jackpot_chance > 50 and self.current_jackpot_chance <= 75:
+            self.ball_color = "red"
             self.cards = list(range(3, 8))
-        elif self.shoot > 75 and self.shoot <= 99:
-            self.ball = "yellow"
+        elif self.current_jackpot_chance > 75 and self.current_jackpot_chance <= 99:
+            self.ball_color = "yellow"
             self.cards = list(range(2, 5))
         else:
-            self.ball = "purple"
+            self.ball_color = "purple"
 
-        self.draw()
+        self.draw_cards()
 
         self.message = "Se sente com sorte?"
-        print(self.ball)
-        print(f"{self.slots[0]} {self.slots[1]}")
-        print("")
+        self.debug("play")
 
     def set_scenario(self):
-        if self.shoot <= 20:
-            self.scenario = 1 #Transit Card Riichi
-        elif self.shoot <= 50:
-            self.scenario = 2 #Seat Struggle Riichi
-        elif self.shoot < 80:
-            self.scenario = 3 #Potty Emergency Riichi
+        if self.result_change_chance == 0 and not self.jackpot_reached:
+            if self.current_jackpot_chance <= 20:
+                self.scenario_id = 1 #Transit Card Riichi
+            elif self.current_jackpot_chance <= 50:
+                self.scenario_id = 2 #Seat Struggle Riichi
+            elif self.current_jackpot_chance < 80:
+                self.scenario_id = 3 #Potty Emergency Riichi
+            else:
+                self.scenario_id = 4 #Friday Night Final Train Riichi
+        elif not self.jackpot_reached:
+            if self.result_change_chance <= 50:
+                self.scenario_id = 5 #Yume Background
+            elif self.result_change_chance <= 75:
+                self.scenario_id = 6 #Amanogawa Cut Scene
+            else:
+                self.scenario_id = 7 #Group Indicators
         else:
-            self.scenario = 4 #Friday Night Final Train Riichi
+            self.scenario_id = 8 #Jackpot scenario
 
     def riichi(self):
         if len(set(self.slots)) == 1 and self.slots[0] != 0:
-            self.jackpot = True
-            self.message = "Seu Sortudo!!"
-            self.tries = 0
-            self.chance = list(range(1,101))
-            self.slots = [0, 0, 0]
+            self.jackpot_reached = True
+            self.debug("riichi")
+            self.set_scenario()
+            self.jackpot_event()
         elif self.slots[0] == 0:
             self.message = "Você precisa apostar se quiser ganhar!"
         else:
             luck = random.choice(range(1, 101))
-            if luck > 90:
-                self.message = "Se sentindo sortudo???"
+            if luck > 30:
+                self.message = "Acha que pode mudar o jogo???"
             else:
                 self.message = "Parece que não foi dessa vez!"
-                self.slots = [0, 0, 0]
+
+    #Reset variables after winning
+    def jackpot_event(self):
+        self.message = "Seu Sortudo!!"
+        self.tries = 0
+        self.increasing_jackpot_chance = list(range(1,101))
+        self.slots = [0, 0, 0]
+        self.debug("jackpot_event")
+
+    def debug(self, called):
+        print("")
+        print(f"Chamado por {called}")
+        print(f"Mensagem: '{self.message}'")
+        print(f"Tentativas: {self.tries}")
+        print(f"Slots: | {self.slots[0]} | {self.slots[1]} | {self.slots[2]} |")
+        print(f"Cor: {self.ball_color}")
+        print(f"Chance de Jackpot: {self.current_jackpot_chance}")
+        print(f"ID do Cenário: {self.scenario_id}")
+        print(f"Chance de Reroll: {self.result_change_chance}")
+        print("")
